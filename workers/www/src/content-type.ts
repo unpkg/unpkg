b75b59ff4e09d7561b8c867f38b5fc53393b310f
Michael Jackson <michael@jackson.us>
2025-03-19 13:59:12 -0700

    Use text/javascript for .cjs and .mjs files
    Fixes #364
    Fixes #350

--- workers/www/src/content-type.ts
+++ workers/www/src/content-type.ts
@@ -2,11 +2,18 @@ import { lookup, mimes } from "mrmime";
 
 mimes.authors = "text/plain";
 mimes.changes = "text/plain";
+mimes.cjs = "text/javascript";
+mimes.cts = "text/typescript";
+mimes["d.cts"] = "text/typescript";
+mimes["d.mts"] = "text/typescript";
+mimes["d.ts"] = "text/typescript";
 mimes.license = "text/plain";
 mimes.makefile = "text/plain";
+mimes.mjs = "text/javascript";
+mimes.mts = "text/typescript";
 mimes.patents = "text/plain";
 mimes.readme = "text/plain";
-mimes.ts = "text/plain";
+mimes.ts = "text/typescript";
 mimes.flow = "text/plain";
 
 const textFiles = /\/?(\.[a-z]*rc|\.git[a-z]*|\.[a-z]*ignore|\.lock)$/i;
