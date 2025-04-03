c1de611ede46c41a1c489b069ae6137ba8c6ff5f
Michael Jackson <michael@jackson.us>
2025-04-02 22:18:11 -0700

    Give unpkg-www-worker more idle time

--- packages/unpkg-www/src/server.ts
+++ packages/unpkg-www/src/server.ts
@@ -1,6 +1,8 @@
 import { handleRequest } from "./request-handler.tsx";
 
 let server = Bun.serve({
+  // Give unpkg-www-worker more time to process requests.
+  idleTimeout: process.env.FLY_APP_NAME === "unpkg-www-worker" ? 30 : 5,
   fetch: handleRequest,
 });
 
