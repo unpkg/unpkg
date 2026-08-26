5baf7dff25978eab72d9f535f26261797a4cbe61
Michael Jackson <michael@jackson.us>
2026-08-26 15:50:19 -0700

    Add X/Open Graph preview cards to www, app, and esm sites

--- packages/unpkg-app/src/components/document.tsx
+++ packages/unpkg-app/src/components/document.tsx
@@ -16,6 +16,8 @@ export function Document({
   subtitle?: string;
   wwwOrigin?: string;
 }): VNode {
+  let fullTitle = subtitle == null ? title : `${title} • ${subtitle}`;
+
   let importMap: ImportMap = {
     imports: {
       preact: new URL("/preact@10.25.4/dist/preact.module.js", wwwOrigin).href,
@@ -31,6 +33,17 @@ export function Document({
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta name="description" content={description} />
         <meta name="color-scheme" content="light dark" />
+
+        <meta property="og:type" content="website" />
+        <meta property="og:site_name" content="UNPKG" />
+        <meta property="og:title" content={fullTitle} />
+        <meta property="og:description" content={description} />
+        <meta property="og:image" content={new URL("/social-card.png", wwwOrigin).href} />
+        <meta property="og:image:width" content="1200" />
+        <meta property="og:image:height" content="630" />
+        <meta property="og:image:alt" content={description} />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:site" content="@unpkg" />
         <meta name="theme-color" content="#ffffff" media="(prefers-color-scheme: light)" />
         <meta name="theme-color" content="#000000" media="(prefers-color-scheme: dark)" />
 
@@ -42,7 +55,7 @@ export function Document({
         <script type="importmap" dangerouslySetInnerHTML={{ __html: JSON.stringify(importMap) }} />
         <script type="module" src={useAsset("assets/scripts.ts")} defer></script>
 
-        <title>{subtitle == null ? title : `${title} • ${subtitle}`}</title>
+        <title>{fullTitle}</title>
 
         <script async src="https://www.googletagmanager.com/gtag/js?id=UA-140352188-1"></script>
         <script
--- packages/unpkg-www/src/components/document.tsx
+++ packages/unpkg-www/src/components/document.tsx
@@ -29,6 +29,17 @@ export function Document({
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="description" content={description} />
         <meta name="color-scheme" content="light dark" />
+
+        <meta property="og:type" content="website" />
+        <meta property="og:site_name" content="UNPKG" />
+        <meta property="og:title" content={title} />
+        <meta property="og:description" content={description} />
+        <meta property="og:image" content={new URL("/social-card.png", origin).href} />
+        <meta property="og:image:width" content="1200" />
+        <meta property="og:image:height" content="630" />
+        <meta property="og:image:alt" content={description} />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:site" content="@unpkg" />
         <meta name="theme-color" content="#ffffff" media="(prefers-color-scheme: light)" />
         <meta name="theme-color" content="#000000" media="(prefers-color-scheme: dark)" />
 
